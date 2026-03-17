53d2fcc1d4e43ef44709da2664e3067924b29688
Deni Junior <deni.junior@flutter.com>
2026-03-17 17:07:46 +0000

    feat: Support Given with any call from test

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const SCENARIO_DOC_REGEX = /^## (Scenario:)\s+?(.+)/;
 export const GIVEN_DOC_REGEX = /^-\s+?\*\*(Given:)\*\*\s+?(.+)/;
 export const STEP_DOC_REGEX = /^-\s+?\*\*(When:|Then:|And:)\*\*\s+?(.+)/;
 export const SCENARIO_TESTS_REGEX = /'(Scenario:[^']*)'/;
-export const GIVEN_TESTS_REGEX = /^\s+?test(?:.skip)?\('(Given:)\s+?(.+)'/;
+export const GIVEN_TESTS_REGEX = /'(Given:)\s?([^']*)'/;
 export const STEP_TESTS_REGEX = /^\s+?\/\/\s(When:|Then:|And:)\s?(.+)/;
